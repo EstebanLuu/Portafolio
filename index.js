d86c888b3253f0b8472cd8213825caad79aac5f6
EstebanLuu <estebaanlunaaa@gmail.com>
2022-07-10 19:00:12 -0300

    Portafolio-primerVersión

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+//funcion que me aplica a la opcion seleccionada en el menu y quita la previamente selecionada
+function seleccionar(link) {
+  let opciones = document.querySelectorAll("#links a");
+  opciones[0].className = "";
+  opciones[1].className = "";
+  opciones[2].className = "";
+  opciones[3].className = "";
+  opciones[4].className = "";
+  opciones[5].className = "";
+  link.className = "seleccionado";
+  let x = document.getElementById("nav");
+}
+
 //responsive menu function
 
 function responsiveMenu() {
