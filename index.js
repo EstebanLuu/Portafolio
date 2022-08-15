55dbc68b11a16c31e774b45f92126de958251aa3
EstebanLuu <estebaanlunaaa@gmail.com>
2022-08-14 23:28:29 -0300

    2.0

--- index.js
+++ index.js
@@ -21,3 +21,23 @@ function responsiveMenu() {
     i.className = "";
   }
 }
+
+const flagsElement = document.getElementById("flags");
+
+const textsToChange = document.querySelectorAll("[data-section]");
+
+const changeLanguage = async (language) => {
+  const requestJson = await fetch(`./languages/${language}.json`);
+  const texts = await requestJson.json();
+
+  for (const textToChange of textsToChange) {
+    const section = textToChange.dataset.section;
+    const value = textToChange.dataset.value;
+
+    textToChange.innerHTML = texts[section][value];
+  }
+};
+
+flagsElement.addEventListener("click", (e) => {
+  changeLanguage(e.target.parentElement.dataset.language);
+});
